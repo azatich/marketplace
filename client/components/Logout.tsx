3c41bdc960448d340b9acc8e4ab0c2a58f20a8ba
Azat <azatabdirashituly@gmail.com>
2025-12-31 15:42:46 +0500

    changed logout button

---
+++ client/components/Logout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useLogout } from "@/features/auth";
+import { Button } from "./ui/button";
+
+const LogoutButton = () => {
+  const { mutate, isPending } = useLogout();
+
+  return (
+    <Button className="bg-red-500 hover:bg-red-900 transition-colors duration-200" onClick={() => mutate()} disabled={isPending}>
+      {isPending ? 'Выходим...' : 'Выйти'}
+    </Button>
+  );
+};
+
+export default LogoutButton;
